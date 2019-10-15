0dfa570ada86cd5b2e57456c7b908ed729d4d8e0
Chuan Cheng <c_chuan23@hotmail.com>
2019-10-15 13:50:16 +0100

    Add bash script to create database and tables

--- migrate_database.sh
+++ migrate_database.sh
@@ -1,3 +1,5 @@
-psql -f ./db/migrations/00_create_database.sql
-psql -f ./db/migrations/01_create_bookmarks_table.sql
-psql -f ./db/migrations/02_create_comments_table.sql
+psql -U chuancheng -f ./db/migrations/00_create_database.sql
+psql -U chuancheng -d bookmark_manager -f ./db/migrations/01_create_bookmarks_table.sql
+psql -U chuancheng -d bookmark_manager_test -f ./db/migrations/01_create_bookmarks_table.sql
+psql -U chuancheng -d bookmark_manager -f ./db/migrations/02_create_comments_table.sql
+psql -U chuancheng -d bookmark_manager_test -f ./db/migrations/02_create_comments_table.sql
