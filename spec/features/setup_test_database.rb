cabee3e9b5ecec06af737d434707a10870f8c596
Chuan Cheng <c_chuan23@hotmail.com>
2019-10-15 16:06:54 +0100

    Change the way tables are reset

--- spec/features/setup_test_database.rb
+++ spec/features/setup_test_database.rb
@@ -2,11 +2,9 @@ require 'pg'
 
 def setup_test_database
   conn = PG.connect(dbname: 'bookmark_manager_test')
-
-  conn.exec('TRUNCATE bookmarks RESTART identity;')
-
-
-
-
-  conn.close
+  conn.exec('DROP TABLE comments;')
+  conn.exec('DROP TABLE bookmarks;')
+  conn.exec('CREATE TABLE bookmarks(id SERIAL PRIMARY KEY, title VARCHAR(60), url VARCHAR(60));')
+  conn.exec('CREATE TABLE comments(id SERIAL PRIMARY KEY, text VARCHAR(240),
+            bookmark_id INTEGER REFERENCES bookmarks (id));')
 end
