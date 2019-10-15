13cbf1dcefbecdfb5cbb442d2de716d447491740
Chuan Cheng <c_chuan23@hotmail.com>
2019-10-15 16:09:41 +0100

    Check existence of table before dropping

--- spec/features/setup_test_database.rb
+++ spec/features/setup_test_database.rb
@@ -2,8 +2,8 @@ require 'pg'
 
 def setup_test_database
   conn = PG.connect(dbname: 'bookmark_manager_test')
-  conn.exec('DROP TABLE comments;')
-  conn.exec('DROP TABLE bookmarks;')
+  conn.exec('DROP TABLE IF EXISTS comments;')
+  conn.exec('DROP TABLE IF EXISTS bookmarks;')
   conn.exec('CREATE TABLE bookmarks(id SERIAL PRIMARY KEY, title VARCHAR(60), url VARCHAR(60));')
   conn.exec('CREATE TABLE comments(id SERIAL PRIMARY KEY, text VARCHAR(240),
             bookmark_id INTEGER REFERENCES bookmarks (id));')
