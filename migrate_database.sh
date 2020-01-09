d06d2a6e9f4fa92cbf3f568e4c2481200dfd49ec
Chuan Cheng <c_chuan23@hotmail.com>
2020-01-09 18:28:42 +0000

    Add a script to set up database

--- migrate_database.sh
+++ migrate_database.sh
@@ -1,5 +1,5 @@
-psql -U chuancheng -f ./db/migrations/00_create_database.sql
-psql -U chuancheng -d bookmark_manager -f ./db/migrations/01_create_bookmarks_table.sql
-psql -U chuancheng -d bookmark_manager_test -f ./db/migrations/01_create_bookmarks_table.sql
-psql -U chuancheng -d bookmark_manager -f ./db/migrations/02_create_comments_table.sql
-psql -U chuancheng -d bookmark_manager_test -f ./db/migrations/02_create_comments_table.sql
+psql -U postgres -f ./db/migrations/00_create_database.sql
+psql -U postgres -d bookmark_manager -f ./db/migrations/01_create_bookmarks_table.sql
+psql -U postgres -d bookmark_manager_test -f ./db/migrations/01_create_bookmarks_table.sql
+psql -U postgres -d bookmark_manager -f ./db/migrations/02_create_comments_table.sql
+psql -U postgres -d bookmark_manager_test -f ./db/migrations/02_create_comments_table.sql
